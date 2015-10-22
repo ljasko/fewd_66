c8f80308bf4eb937461920fb27be4b7caaf349dc
ljasko <ljasko@gmail.com>
2015-10-21 21:31:00 -0400

    SASS & This labs

---
+++ 10-21-2015/labs/this/js/script.js
@@ -0,0 +1,12 @@
+$('li').on('click', function() {
+	var textTitle = $(this).data('book');
+	var textHref = $(this).data('href');
+
+	var html = '<div class="popup">';
+		html += '<h3>' + textTitle + '</h3>';
+		html += '<a href="' + textHref + '">READ MORE</a>';
+		html += '</div>';
+
+	$('.popup').remove();
+	$(this).append(html);
+});
